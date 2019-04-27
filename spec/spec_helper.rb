93e875a970984be5097f4aaa8b2e59ec3d4c3958
iain barnett <iainspeed@gmail.com>
2019-04-27 16:09:12 +0900

    These settings were interfering so set them off.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,11 +7,9 @@ Pathname(__dir__).join("support").glob "**/*.rb" do |rb|
   require rb
 end
 
+
 RSpec.configure do |config|
   Pry.config.extra_sticky_locals = {}
-#   config.before(:pry) do
-#   end
-#
-#   config.before(:pry) do
-#   end
+  Pry.config.should_load_rc = false
+  Pry.config.pager = false
 end
